21467b57587c0398272375ad112f92c483c95d28
jdiegoisaza <jdiego.isaza@PC-LIS>
2018-05-11 13:47:17 -0500

    int

--- Practica2/src/practica2/Practica2.java
+++ Practica2/src/practica2/Practica2.java
@@ -5,6 +5,8 @@
  */
 package practica2;
 
+import java.util.StringTokenizer;
+
 /**
  *
  * @author jdiego.isaza
@@ -17,6 +19,28 @@ public class Practica2 {
     public static void main(String[] args) {
         // TODO code application logic here
         int n=0;
+        
+        String s="1. A = aC\n" +
+                 "2. A = bBc\n" +
+                 "3. B = aA\n" +
+                 "4. B = bB\n" +
+                 "5. C = aCa\n" +
+                 "6. C = bBcc\n" +
+                 "7. C = |";
+        
+        StringTokenizer st = new StringTokenizer(s, "\n");
+        while (st.hasMoreTokens()) {
+            System.out.println(st.nextToken().charAt(3));
+            
+        }
+        
+    }
+    public void reconocedor (String s){
+            
+        StringTokenizer st = new StringTokenizer("this is a test");
+        while (st.hasMoreTokens()) {
+            System.out.println(st.nextToken());
+        }
     }
     
 }
